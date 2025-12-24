597181d3e16ff51f20dee684b31c631bbf6ee49d
PedroAfonso122 <cuser0004@gmail.com>
2025-12-24 02:44:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ const btnVoltar = document.getElementById("voltar");
 
 const textos = [
     "Isadora,\n\nJá escrevi palavras para você pelas cartinhas.\nMas dessa vez não vai bastar só palavras, algumas coisas merecem mais tempo, mais cuidado e mais sentimento.\n\nEsse presente veio do meu  coração, feito do jeito mais sincero que eu consegui.\n\nTalvez você se emocione.\nTalvez você se surpreenda.\nTalvez sorria.\nMas me prometa uma coisa:\nLeia com calma.\nPorque fiz sobre nós.",
-    "Essa foto guarda um dos momentos mais intensos para mim.\nEnquanto eu segurava e olhava para as alianças, meu coração estava acelerado.\nEu estava feliz, nervoso... e cheio de dúvidas.\n\nEu pensava se era muito cedo.\nSe eu estava pronto.\nSe fazia sentido.\nMas eu sabia que teria que tentar.\nEu ja estava feliz em escolher você.\nPorque uma certeza maior do que tudo que eu tinha:\nEra que meu coração já era seu, que era você...\nComprar essas alianças não foi só uma decisão.\nFoi um sentimento.",
-    "Eu nunca vou esquecer desse dia.\nCada segundo antes do pedido foi ansiedade.\nCada segundo depois do seu SIM foi felicidades.\n\nQuando você disse sim, eu senti paz.\nFoi ali que eu tive certeza de que eu estava com a pessoa certa...",
-    "Esse dia foi especial por um motivo simples, mas profundo para mim.\nEu me senti parte.\nMe senti incluído.\nMe senti escolhido.\nQuando você disse:\n'Se me chamou para um lugar, chamou você também',\nEu senti algo muito bonito.\nNaquele momento eu entendi.\nEstar com você é pertencer.\nE essa foto guarda exatamente isso.",
-    "Esse momento parece simples.\nMas pra mim, ele significou muito.\nVocê estava ali, cuidando de mim.\nCom calma, com atenção, com carinho.\nNesse dia eu senti algo diferente.\nUma certeza.\n\nCom você eu me sinto bem cuidado.\nMe sinto seguro.\nE feliz de um jeito leve.\n\nAlgo que eu nunca tinha sentido antes.",
+    "Essa foto guarda um dos momentos mais intensos para mim.\n\nEnquanto eu segurava e olhava para as alianças, meu coração estava acelerado.\nEu estava feliz, nervoso... e cheio de dúvidas.\n\nEu pensava se era muito cedo.\nSe eu estava pronto.\nSe fazia sentido.\n\nMas eu sabia que teria que tentar.\nEu ja estava feliz em escolher você.\nPorque uma certeza maior do que tudo que eu tinha:\nEra que meu coração já era seu, que era você...\n\nComprar essas alianças não foi só uma decisão.\nFoi um sentimento.",
+    "Eu nunca vou esquecer desse dia.\n\nCada segundo antes do pedido foi ansiedade.\nCada segundo depois do seu SIM foi felicidades.\n\nQuando você disse sim, eu senti paz.\n\nFoi ali que eu tive certeza de que eu estava com a pessoa certa...",
+    "Esse dia foi especial por um motivo simples, mas profundo para mim.\n\nEu me senti parte.\nMe senti incluído.\nMe senti escolhido.\n\nQuando você disse:\n'Se me chamou para um lugar, chamou você também',\nEu senti algo muito bonito.\n\nNaquele momento eu entendi.\nEstar com você é pertencer.\nE essa foto guarda exatamente isso.",
+    "Esse momento parece simples.\nMas pra mim, ele significou muito.\n\nVocê estava ali, cuidando de mim.\nCom calma, com atenção, com carinho.\n\nNesse dia eu senti algo diferente.\nUma certeza.\n\nCom você eu me sinto bem cuidado.\nMe sinto seguro.\nE feliz de um jeito leve.\n\nAlgo que eu nunca tinha sentido antes.",
     "Existe algo em você que sempre me marcou.\n\nAntes mesmo de voce dizer que me amava, eu já sentia o mesmo.\nNa forma como você pensa no futuro.\nNa dedicação que você tem com tudo que faz.\nNa sua força.\n\nVocê é esforçada, inteligente, bondosa.\nVocê sabe o que quer.\nVocê luta.\nE não espera que ninguém faça por você.\n\nVocê cuida da sua casa nos mínimos detalhes.\nCuida do que é seu.\nCuida de quem está ao seu lado.\n\nVocê é de poucas amizades, mas de sentimentos verdadeiros.\nVocê odeia mentiras.\nE isso me faz admirar ainda mais quem você é.\n\nQuando você disse 'eu te amo' primeiro,\nAquilo significou muito pra mim.\nSignificou confiança.\nEntrega.\nVerdade.\n\nHoje, tudo que eu quero é caminhar ao seu lado.\nMe tornar um homem que te traga segurança.\nQue te faça sentir protegida.\nQue te faça se sentir feminina.\n\nNão porque você precise.\nMas porque você merece.\n\nEu quero crescer com você.\nAprender com você.\nConstruir com você.\n\nVocê é a minha primeira namorada.\nA primeira mulher para quem eu entreguei um buquê de flores.\nA primeira para quem eu dei um presente pensando em cada detalhe.\nE vai ser a única.\nVocê é incrível Isadora, sou grato por ter você na minha vida.\n\nTudo isso foi novo para mim.\nMas tudo fez sentido com você.\nPorque desde o começo, foi verdadeiro.",
     "EU TE AMOO MINHA CHEIROSAAAAAAA"
 ];
@@ -41,30 +41,25 @@ function digitarTexto(indice) {
 
     if (intervaloDigitar) clearInterval(intervaloDigitar);
 
-    const paragrafo = textosHTML[indice];
-
-    paragrafo.textContent = "";
+    textosHTML[indice].textContent = "";
     let i = 0;
     const velocidade = indice === 5 ? 160 : 120;
+    const etapaElemento = textosHTML[indice].parentElement;
 
     intervaloDigitar = setInterval(() => {
         if (i < textos[indice].length) {
-            paragrafo.textContent += textos[indice][i];
-            i++;
-
-            // ✅ SCROLL QUE NÃO FALHA
-            paragrafo.scrollIntoView({
-                block: "end",
-                behavior: "smooth"
-            });
-
+           textosHTML[indice].textContent += textos[indice][i];
+           i++;
+           etapaElemento.scrollTop = etapaElemento.scrollHeight;
         } else {
             clearInterval(intervaloDigitar);
             intervaloDigitar = null;
-            textosDigitados[indice] = paragrafo.textContent;
+            textosDigitados[indice] = textosHTML[indice].textContent;
         }
     }, velocidade);
 }
+
+
 function tocarMusica(indice) {
     const novaMusica = trilha[indice].src;
 
@@ -123,8 +118,3 @@ function fadeOutMusica() {
         }
     }, 200);
 }
-document.addEventListener("click", () => {
-    if (musica.paused) {
-        musica.play().catch(() => {});
-    }
-}, {once: true});
